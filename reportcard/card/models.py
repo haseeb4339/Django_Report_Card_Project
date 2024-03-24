6666cd0fc7bf70771942693dd4479ec1d2a2b0d4
haseeb4339 <haseebullahbj@gmail.com>
2024-03-24 05:44:10 +0500

    add str fucntion to subject model

--- reportcard/card/models.py
+++ reportcard/card/models.py
@@ -26,6 +26,11 @@ class Subject(models.Model):
 
     subject_name = models.CharField(max_length=100)
 
+    def __str__(self):
+
+        return self.subject_name
+    
+
 
 
 class Student(models.Model):
