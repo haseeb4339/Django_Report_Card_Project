32971bc835458f28e0618975ffec5b423f908593
haseeb4339 <haseebullahbj@gmail.com>
2024-03-25 22:55:36 +0500

    add multipler query filter

--- reportcard/card/views.py
+++ reportcard/card/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Student
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 
@@ -10,7 +11,10 @@ def get_students(request):
     if request.GET.get('search'):
         search = request.GET.get('search')
 
-        queryset = queryset.filter(student_name__icontains = search)
+        queryset = queryset.filter(Q(student_name__icontains = search)|
+                                   Q(department__department__icontains=search)|
+                                   Q(student_id__stud_id__icontains=search)|
+                                   Q(student_email__icontains=search))
 
     paginator = Paginator(queryset, 2)  # Show 25 contacts per page.
 
